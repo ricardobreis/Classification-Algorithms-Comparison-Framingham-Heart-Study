20797022eff708d641995bea290eb14f259de5e0
Ricardo Reis <ricardo.l.b.reis@gmail.com>
2019-07-13 15:15:32 -0300

    Update modelo.r
    Descrevendo variáveis.

--- modelo.r
+++ modelo.r
@@ -5,6 +5,23 @@
 #
 # CASE - FRAMINGHAM HEART STUDY
 #
+# male: 0 = Female; 1 = Male
+# age: Age at exam time.
+# education: 1 = Some High School; 2 = High School or GED; 3 = Some College or Vocational School; 4 = college
+# currentSmoker: 0 = nonsmoker; 1 = smoker
+# cigsPerDay: number of cigarettes smoked per day (estimated average)
+# BPMeds: 0 = Not on Blood Pressure medications; 1 = Is on Blood Pressure medications
+# prevalentStroke
+# prevalentHyp
+# diabetes: 0 = No; 1 = Yes
+# totChol: mg/dL
+# sysBP: mmHg
+# diaBP: mmHg
+# BMI: Body Mass Index calculated as: Weight (kg) / Height(meter-squared)
+# heartRate: Beats/Min (Ventricular)
+# glucose: mg/dL
+# TenYearCHD
+#
 ################################################################################################
 # LENDO OS DADOS
 path <- "C:/Users/Ricardo/Documents/R-Projetos/FraminghamHeartStudy/"
