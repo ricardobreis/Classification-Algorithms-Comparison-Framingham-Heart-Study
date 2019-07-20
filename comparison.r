6f3367ff504a56c1b4f324cde0b326b18be7f492
Ricardo Reis <ricardo.l.b.reis@gmail.com>
2019-07-20 16:37:30 -0300

    Update comparison.r
    Adicionando legenda.

--- comparison.r
+++ comparison.r
@@ -4,3 +4,5 @@ plot(x1,y1, type="n",
 lines(x1, y1,lwd=3,lty=1, col="purple")
 lines(x2, y2,lwd=3,lty=1, col="red")
 abline(0,1, lty=2)
+legend(0.6, 0.2, legend=c("Árvore de Classificação", "Regressão Logística"),
+       col=c("red", "purple"), lty=1:1, cex=0.8)
