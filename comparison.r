673567caa9eb63cca216b3ba2a59c3656cdda28c
Ricardo Reis <ricardo.l.b.reis@gmail.com>
2019-07-25 13:20:37 -0300

    Update comparison.r
    Gerando a curva ROC com os 4 modelos.

--- comparison.r
+++ comparison.r
@@ -3,6 +3,8 @@ plot(x1,y1, type="n",
      ylab= "Sensitividade")
 lines(x1, y1,lwd=3,lty=1, col="purple")
 lines(x2, y2,lwd=3,lty=1, col="red")
+lines(x3, y3,lwd=3,lty=1, col="pink") 
+lines(x4, y4,lwd=3,lty=1, col="blue") 
 abline(0,1, lty=2)
-legend(0.6, 0.2, legend=c("Árvore de Classificação", "Regressão Logística"),
-       col=c("red", "purple"), lty=1:1, cex=0.8)
+legend(0.8, 0.3, legend=c("Árvore de Classificação", "Regressão Logística", 'Random Forest', "Boosting"),
+       col=c("red", "purple", "pink", "blue"), lty=1:1, cex=0.8)
